e7b8955d8891fe22adacf3ba7ae43b1d4e009b86
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-06-26 16:48:33 +0200

    Make separate main for different running purpose

--- app.py
+++ app.py
@@ -1,24 +1,4 @@
-from Slave.slave import Slave
-from queue import *
-
 if __name__ == "__main__":
-    print("=============Testing Slave=============")
-    q = Queue()
-    for i in range(4):
-        task = {}
-        task["groupid"] = str(i)
-        task["dataset"] = "test"
-        q.put(task)
-
-    for i in range(5):
-        task = {}
-        task["groupid"] = str(i)
-        task["dataset"] = "test"
-        q.put(task)
-
-    s = Slave(1,q,4,name="Slave1")
-    s.start()
-    s.join()
-    print("=============End of Slave Testing=============")
+    print("This is main")
 
 
