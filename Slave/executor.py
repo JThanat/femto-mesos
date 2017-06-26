519b2345f629b2c15ae023ad4f1342f0baedea3f
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-06-26 16:27:00 +0200

    Add Executor class to use callback style from parent

--- Slave/executor.py
+++ Slave/executor.py
@@ -1,30 +1,43 @@
 import threading
 import time
 import logging
-from random import random
+from random import *
+
+
+class Executor(threading.Thread):
+
+    def __init__(self, parent=None, **kwargs):
+        self.parent = parent
+        super(Executor, self).__init__(**kwargs)
+
+    def run(self):
+        self.parent.allocate()
+        super(Executor, self).run()
+        self.parent.release()
+        self.parent.notify_master()
+
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-10s) %(message)s',
+                    format="(%(threadName)-10s) %(message)s",
                     )
-
 
 def execute_with_cache(cache_data):
     """thread worker function with cache"""
     t = threading.currentThread()
-    pause = random() * 10
-    logging.debug('working with', str(cache_data))
+    pause = randint(5,10)
+    logging.debug("working with %s", str(cache_data))
     time.sleep(pause)
-    logging.debug('finish')
+    logging.debug("finish")
 
 
-def fetch_and_execute(cache, key: str):
+def fetch_and_execute(cache, key):
     """thread worker function for worker without cache"""
     t = threading.currentThread()
-    fetching_time = random() * 5
+    fetching_time = randint(3,5)
     time.sleep(fetching_time)
-    logging.debug('fetching %s', key)
+    logging.debug("fetching %s", key)
     cache[key] = key
-    execution_time = random()
+    execution_time = randint(5,10)
     time.sleep(execution_time)
-    logging.debug('executing %s', cache['key'])
-    logging.debug('finish')
+    logging.debug("executing %s", cache[key])
+    logging.debug("finish")
