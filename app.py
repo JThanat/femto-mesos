20d3b40746981635560a0fa45da7fd5a836ec85b
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-06-26 16:31:46 +0200

    Add code for Slave Test running

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
-if __name__ == '__main__':
-    print "This is main"
+from Slave.slave import Slave
+from queue import *
+
+if __name__ == "__main__":
+
+    print("=============Testing Slave=============")
+    q = Queue()
+    for i in range(4):
+        task = {}
+        task["groupid"] = str(i)
+        task["dataset"] = "test"
+        q.put(task)
+
+    for i in range(5):
+        task = {}
+        task["groupid"] = str(i)
+        task["dataset"] = "test"
+        q.put(task)
+
+    s = Slave(1,q,4,name="Slave1")
+    s.start()
+    s.join()
+    print("=============End of Slave Testing=============")
 
