74d3059be04e1ada9fc732b3e6be4397f335557a
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-06-29 15:52:13 +0200

    Refactor Test File

--- framework/framework.py
+++ framework/framework.py
@@ -1,12 +1,10 @@
 import logging
-import threading
-import time
-import uuid
-from random import randint
 
-from framework.task import Task
+import mesos.native
+from mesos.interface import mesos_pb2
 
 TOTAL_TASKS = 5
+
 TASK_CPUS = 1
 TASK_MEM = 128
 
@@ -15,100 +13,143 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 
 
-class Dispatcher():
-    # This class defines the framework of Mesos or Job Dispatcher of the system
+class Dispatcher(mesos.interface.Scheduler):
     def __init__(self, implicitAcknowledgements, executor):
+        self.implicitAcknowledgements = implicitAcknowledgements
         self.executor = executor
-        self.task_data = {}
+        self.taskData = {}
         self.tasksLaunched = 0
         self.tasksFinished = 0
         self.messagesSent = 0
         self.messagesReceived = 0
 
-    def registered(self, driver, framework_id, master_info):
-        logging.debug("Registered with framework ID %s" % framework_id.value)
+    def registered(self, driver, frameworkId, masterInfo):
+        print
+        "Registered with framework ID %s" % frameworkId.value
 
-    def resource_offers(self, driver, offers):
-        # the resourceOffers method, where we can process the incoming offers from Mesos and potentially use them
-        # to launch tasks.
+    def resourceOffers(self, driver, offers):
         for offer in offers:
             tasks = []
-            offer_cpus = 0
-            offer_mem = 0
+            offerCpus = 0
+            offerMem = 0
             for resource in offer.resources:
-                if resource.cpu == "cpus":
-                    offer_cpus += offer_cpus
+                if resource.name == "cpus":
+                    offerCpus += resource.scalar.value
                 elif resource.name == "mem":
-                    offer_mem += offer_mem
+                    offerMem += resource.scalar.value
 
-            logging.debug(
-                "Received offer {0} with cpus: {1} and mem: {2}".format(offer.id.value, offer_cpus, offer_mem))
+            print
+            "Received offer %s with cpus: %s and mem: %s" \
+            % (offer.id.value, offerCpus, offerMem)
 
-            remaining_cpus = offer_cpus
-            remaining_mem = offer_mem
+            remainingCpus = offerCpus
+            remainingMem = offerMem
 
-            while self.tasksLaunched < TOTAL_TASKS and remaining_cpus >= TASK_CPUS and remaining_mem >= TASK_MEM:
+            while self.tasksLaunched < TOTAL_TASKS and \
+                            remainingCpus >= TASK_CPUS and \
+                            remainingMem >= TASK_MEM:
                 tid = self.tasksLaunched
                 self.tasksLaunched += 1
 
-                # really launch some task and keep track of the value
-                logging.debug("Launching task {0} using offer {1}".format(tid, offer.id.value))
-                task = self.new_task(offer)
-                tasks.append(task)
+                print
+                "Launching task %d using offer %s" \
+                % (tid, offer.id.value)
 
-                remaining_cpus -= TASK_CPUS
-                remaining_mem -= TASK_MEM
+            task = mesos_pb2.TaskInfo()
+            task.task_id.value = str(tid)
+            task.slave_id.value = offer.slave_id.value
+            task.name = "task %d" % tid
+            task.executor.MergeFrom(self.executor)
 
-                self.launch_tasks(offer.id, tasks)
+            cpus = task.resources.add()
+            cpus.name = "cpus"
+            cpus.type = mesos_pb2.Value.SCALAR
+            cpus.scalar.value = TASK_CPUS
 
-                # Mesos
-                # operation = mesos_pb2.Offer.Operation()
-                # operation.type = mesos_pb2.Offer.Operation.LAUNCH
-                # operation.launch.task_infos.extend(tasks)
-                # driver.acceptOffers([offer.id], [operation])
-                # or
-                # driver.launchTasks(offer.id, tasks)
+            mem = task.resources.add()
+            mem.name = "mem"
+            mem.type = mesos_pb2.Value.SCALAR
+            mem.scalar.value = TASK_MEM
 
-    def new_task(self, offer):
-        # For Mesos
-        # task = mesos_pb2.TaskInfo()
-        # task.task_id.value = str(tid)
-        # task.slave_id.value = offer.slave_id.value
-        # task.name = "task %d" % tid
-        # task.executor.MergeFrom(self.executor)
-        #
-        # cpus = task.resources.add()
-        # cpus.name = "cpus"
-        # cpus.type = mesos_pb2.Value.SCALAR
-        # cpus.scalar.value = TASK_CPUS
-        #
-        # mem = task.resources.add()
-        # mem.name = "mem"
-        # mem.type = mesos_pb2.Value.SCALAR
-        # mem.scalar.value = TASK_MEM
+            tasks.append(task)
+            self.taskData[task.task_id.value] = (
+                offer.slave_id, task.executor.executor_id)
 
-        tid = uuid.uuid4()
-        task = Task(tid, offer, self.executor)
-        return task
+            remainingCpus -= TASK_CPUS
+            remainingMem -= TASK_MEM
 
-    def offerRescinded(self, driver, offerId):
-        # stop the task if the offer is no longer valid
-        pass
+        operation = mesos_pb2.Offer.Operation()
+        operation.type = mesos_pb2.Offer.Operation.LAUNCH
+        operation.launch.task_infos.extend(tasks)
 
-    def statusUpdate(self, driver, update):
-        # For Mesos
-        # logging.debug("Task {0} is in state {1}".format(update.task_id.value, mesos_pb2.TaskState.Name(update.state)))
-        logging.debug("Task {0} is in state {1}".format(update.task_id, update.state))
-
-    def launch_task(self, offer_id, tasks):
-        # Should be deleted soon
-        for task in tasks:
-            t = threading.Thread(target=run_task, name="OfferId {}".format(str(offer_id)),
-                                 args=[offer_id, task.task_id], daemon=True)
+        driver.acceptOffers([offer.id], [operation])
 
 
-def run_task(offer_id, task_id):
-    logging.debug("Task {0} is being executed by offer_id {1}").format(task_id, offer_id)
-    execution_time = randint(2, 5)
-    time.sleep(execution_time)
-    logging.debug("finish")
+def statusUpdate(self, driver, update):
+    print
+    "Task %s is in state %s" % \
+    (update.task_id.value, mesos_pb2.TaskState.Name(update.state))
+
+    # Ensure the binary data came through.
+    if update.data != "data with a \0 byte":
+        print
+        "The update data did not match!"
+        print
+        "  Expected: 'data with a \\x00 byte'"
+        print
+        "  Actual:  ", repr(str(update.data))
+        sys.exit(1)
+
+    if update.state == mesos_pb2.TASK_FINISHED:
+        self.tasksFinished += 1
+        if self.tasksFinished == TOTAL_TASKS:
+            print
+            "All tasks done, waiting for final framework message"
+
+        slave_id, executor_id = self.taskData[update.task_id.value]
+
+        self.messagesSent += 1
+        driver.sendFrameworkMessage(
+            executor_id,
+            slave_id,
+            'data with a \0 byte')
+
+    if update.state == mesos_pb2.TASK_LOST or \
+                    update.state == mesos_pb2.TASK_KILLED or \
+                    update.state == mesos_pb2.TASK_FAILED:
+        print
+        "Aborting because task %s is in unexpected state %s with message '%s'" \
+        % (update.task_id.value, mesos_pb2.TaskState.Name(update.state), update.message)
+        driver.abort()
+
+    # Explicitly acknowledge the update if implicit acknowledgements
+    # are not being used.
+    if not self.implicitAcknowledgements:
+        driver.acknowledgeStatusUpdate(update)
+
+
+def frameworkMessage(self, driver, executorId, slaveId, message):
+    self.messagesReceived += 1
+
+    # The message bounced back as expected.
+    if message != "data with a \0 byte":
+        print
+        "The returned message data did not match!"
+        print
+        "  Expected: 'data with a \\x00 byte'"
+        print
+        "  Actual:  ", repr(str(message))
+        sys.exit(1)
+    print
+    "Received message:", repr(str(message))
+
+    if self.messagesReceived == TOTAL_TASKS:
+        if self.messagesReceived != self.messagesSent:
+            print
+            "Sent", self.messagesSent,
+            print
+            "but received", self.messagesReceived
+            sys.exit(1)
+        print
+        "All tasks done, and all messages received, exiting"
+        driver.stop()
