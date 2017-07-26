ed4067827610233ec713a5cf8dce1a2b2d9c1ea6
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-07-26 18:13:10 +0200

    Add job_path to Executor for further usage

--- compute/executor.py
+++ compute/executor.py
@@ -1,30 +1,33 @@
+import logging
 import threading
 import time
-import logging
 from random import *
+
+from framework.job import Jobstate
 
 
 class Executor(threading.Thread):
-
-    def __init__(self, parent=None, **kwargs):
+    def __init__(self, job_path, parent=None, **kwargs):
         self.parent = parent
+        self.job_path = job_path
         super(Executor, self).__init__(**kwargs)
 
     def run(self):
         self.parent.allocate()
         super(Executor, self).run()
+        self.parent.update_state(Jobstate.SUCCESSFUL)
         self.parent.release()
-        self.parent.notify_master()
 
 
 logging.basicConfig(level=logging.DEBUG,
                     format="(%(threadName)-10s) %(message)s",
                     )
 
+
 def execute_with_cache(cache_data):
     """thread worker function with cache"""
     t = threading.currentThread()
-    pause = randint(5,10)
+    pause = randint(5, 10)
     logging.debug("working with %s", str(cache_data))
     time.sleep(pause)
     logging.debug("finish")
@@ -33,11 +36,11 @@ def execute_with_cache(cache_data):
 def fetch_and_execute(cache, key):
     """thread worker function for worker without cache"""
     t = threading.currentThread()
-    fetching_time = randint(3,5)
+    fetching_time = randint(3, 5)
     time.sleep(fetching_time)
     logging.debug("fetching %s", key)
     cache[key] = key
-    execution_time = randint(5,10)
+    execution_time = randint(5, 10)
     time.sleep(execution_time)
     logging.debug("executing %s", cache[key])
     logging.debug("finish")
