0c644515f310aaa954005a98975140d3c7122db7
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-08-03 14:40:09 +0200

    Change Client order

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
-from kazoo.client import KazooClient
 import sys
+from kazoo.client import KazooClient
+from compute.zk_compute import *
+from dispatcher.dispathcer import Dispatcher
+from dispatcher.dispathcer import Watcher
 
 enable_create = sys.argv[1]
 enable_dispatcher = sys.argv[2]
 enable_slave = sys.argv[3]
+enable_watcher = sys.argv[4]
 
 zk = KazooClient(hosts='127.0.0.1:2181')
 zk.start()
+
+slave = Slave(zk,"/")
+dispatcher = Dispatcher(client=zk)
+watcher = Watcher(zk, dispatcher.work_queue)
+
 
 if enable_create == '1':
     zk.create('owned', "owned branch")
@@ -14,14 +23,10 @@ if enable_create == '1':
     zk.create('done', "done branch")
 
 if enable_dispatcher == '1':
-    from dispatcher.dispathcer import Dispatcher
-
     prefix = "entry-"
     priority = 100
 
-    dispatcher = Dispatcher(client=zk)
     dispatcher.start()
-
     work_queue = dispatcher.work_queue
 
     for i in range(8):
@@ -29,7 +34,6 @@ if enable_dispatcher == '1':
         d["dataset"] = i % 4
         d["groupid"] = i % 4
         work_queue.put(d)
-        # dispatcher.join()
 
         # d["state"] = Jobstate.PENDING
         # d["worker_node"] = None
@@ -43,8 +47,16 @@ if enable_dispatcher == '1':
         # )
 
 if enable_slave == '1':
-    from compute.zk_compute import *
-
-    slave = Slave(zk,"/")
     slave.start()
+
+if enable_watcher == '1':
+    watcher.start()
+
+if enable_dispatcher == '1':
+    dispatcher.join()
+
+if enable_slave == '1':
     slave.join()
+
+if enable_watcher == '1':
+    watcher.join()
