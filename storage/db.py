add8d2eac1bd254f51497a760436fa1af17208de
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-08-04 19:48:32 +0200

    Add storage Package

---
+++ storage/db.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+
+class MongoInitializer(object):
+    HOST = 'localhost'
+    PORT = 27917
+
+    def __init__(self, host=HOST, port=PORT, database_name="result_database", collectioname="results"):
+        self.mongo_client = MongoClient(host, port)
+        self.db = self.mongo_client[database_name]
+        self.collection = self.db[collectioname]
+
+    def get_from_key(self, dataset, groupid):
+        job_mongo = self.collection.find_one({"dataset": dataset,
+                                              "groupid": groupid
+                                              })
